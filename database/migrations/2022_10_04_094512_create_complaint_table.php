663574a282bf615dffac04eb54a21e50bafda206
Anisaazz <78440211+Anisaazz@users.noreply.github.com>
2022-10-04 18:05:37 +0700

    Create new table complaint

---
+++ database/migrations/2022_10_04_094512_create_complaint_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComplaintTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('complaint', function (Blueprint $table) {
+            $table->id('complaint_id');
+            $table->string('complaint_name', 255);
+            $table->string('code_request', 255);
+            $table->string('mps_user', 255);
+            $table->string('main_menu', 255);
+            $table->string('categories', 255);
+            $table->string('other_categories', 255);
+            $table->text('description');
+            $table->string('request', 255);
+            $table->string('reason', 255);
+            $table->string('picture', 255);
+            $table->string('complaint_source_division', 255);
+            $table->string('complaint_classification', 255);
+            $table->string('complaint_sub_classification', 255);
+            $table->string('complaint_pic', 255);
+            $table->string('complaint_treatment', 255);
+            $table->string('complaint_user_input', 255);
+            $table->string('complaint_status', 255);
+            $table->string('complaint_status_code', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('complaint');
+    }
+}
