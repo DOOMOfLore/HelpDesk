e63596316f9076d4ae71fe9913ff1a07a121897f
Anisaazz <78440211+Anisaazz@users.noreply.github.com>
2022-10-05 13:22:00 +0700

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('auth.login');
+});
 
 Route::group(['middleware' => ['auth','prevent-back-history']], function () {
-
-    Route::get('/', function () {
-        return view('auth.login');
-    });
 
     Auth::routes();
 
