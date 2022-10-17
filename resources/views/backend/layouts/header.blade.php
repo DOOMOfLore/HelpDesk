55de053151a0f30c6bfac18948ba3c8a34c9774e
DoomOfLore <yusufahmadghazali@gmail.com>
2022-10-17 13:39:35 +0700

    fix route dashboard helpdesk

--- resources/views/backend/layouts/header.blade.php
+++ resources/views/backend/layouts/header.blade.php
@@ -39,7 +39,7 @@
           <span class="middle"></span>
           <span class="bottom"></span>
         </div>
-        <a href="index.html" class="navbar-brand">
+        <a href="{{ route('dashboard.index') }}" class="navbar-brand">
           <b>Help Desk</b>
         </a>
 
